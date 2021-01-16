1a221b4976abee9c83b8856ca9224b9cc09263a3
vkhomyn95 <vkhomyn95@gmail.com>
2021-01-16 20:46:54 +0200

    described solution filter json

--- src/app/constants/nav-filter/solutionFilter.ts
+++ src/app/constants/nav-filter/solutionFilter.ts
@@ -2,71 +2,85 @@ export const navListSolutionBrand = [
   {
     id: 0,
     name: 'Acuvue',
+    col: 'solutionBrand',
     category: 2
   },
   {
     id: 1,
     name: 'Gelone',
+    col: 'solutionBrand',
     category: 2
   },
   {
     id: 2,
     name: 'Laim Care',
+    col: 'solutionBrand',
     category: 2
   },
   {
     id: 3,
     name: 'AO Sept',
+    col: 'solutionBrand',
     category: 2
   },
   {
     id: 4,
     name: 'AQ Pure',
+    col: 'solutionBrand',
     category: 2
   },
   {
     id: 5,
     name: 'All in One',
+    col: 'solutionBrand',
     category: 2
   },
   {
     id: 6,
     name: 'Alvera',
+    col: 'solutionBrand',
     category: 2
   },
   {
     id: 7,
     name: 'Avizor',
+    col: 'solutionBrand',
     category: 2
   },
   {
     id: 8,
     name: 'Biotrue',
+    col: 'solutionBrand',
     category: 2
   },
   {
     id: 9,
     name: 'Horien',
+    col: 'solutionBrand',
     category: 2
   },
   {
     id: 10,
     name: 'Hy-Care',
+    col: 'solutionBrand',
     category: 2
   },
   {
     id: 11,
     name: 'Opti-Free',
+    col: 'solutionBrand',
     category: 2
   },
   {
     id: 12,
     name: 'ReNu',
+    col: 'solutionBrand',
     category: 2
   },
   {
     id: 13,
     name: 'Solocare',
+    col: 'solutionBrand',
     category: 2
   }
 ];
@@ -74,11 +88,13 @@ export const navListSolutionType = [
   {
     id: 0,
     name: 'Мультифункціональний',
+    col: 'solutionType',
     category: 2
   },
   {
     id: 1,
     name: 'Пероксид',
+    col: 'solutionType',
     category: 2
   }
 ];
@@ -86,42 +102,115 @@ export const navListSolutionValue = [
   {
     id: 0,
     value: 120,
+    col: 'solutionValue',
     category: 2
   },
   {
     id: 1,
     value: 150,
+    col: 'solutionValue',
     category: 2
   },
   {
     id: 2,
     value: 300,
+    col: 'solutionValue',
     category: 2
   },
   {
     id: 3,
     value: 350,
+    col: 'solutionValue',
     category: 2
   },
   {
     id: 4,
     value: 355,
+    col: 'solutionValue',
     category: 2
   },
   {
     id: 5,
     value: 360,
+    col: 'solutionValue',
     category: 2
   },
   {
     id: 6,
     value: 400,
+    col: 'solutionValue',
     category: 2
   }
 ];
 export const navListBool = [
   {
     id: 0,
-    name: 'Гіалуронат натрію'
+    displayName: 'Гіалуронат натрію',
+    col: 'boolHyaluronate',
+    name: true,
+    category: 2
+  }
+];
+
+export const navListSolutionProducer = [
+  {
+    id: 0,
+    name: 'Soleko',
+    col: 'solutionProducer',
+    category: 2
+  },
+  {
+    id: 1,
+    name: 'CooperVision',
+    col: 'solutionProducer',
+    category: 2
+  },
+  {
+    id: 2,
+    name: 'Johnson and Johnson',
+    col: 'solutionProducer',
+    category: 2
+  },
+  {
+    id: 3,
+    name: 'Bausch and Lomb',
+    col: 'solutionProducer',
+    category: 2
+  },
+  {
+    id: 4,
+    name: 'Alcon',
+    col: 'solutionProducer',
+    category: 2
+  },
+  {
+    id: 5,
+    name: 'Schalcon',
+    col: 'solutionProducer',
+    category: 2
+  },
+  {
+    id: 6,
+    name: 'Carl Zeiss',
+    col: 'solutionProducer',
+    category: 2
+  },
+  {
+    id: 7,
+    name: 'Menicon',
+    col: 'solutionProducer',
+    category: 2
+  },
+  {
+    id: 8,
+    name: 'Amo',
+    col: 'solutionProducer',
+    category: 2
+  },
+  {
+    id: 9,
+    name: 'Avizor',
+    col: 'solutionProducer',
+    category: 2
   }
 ];
